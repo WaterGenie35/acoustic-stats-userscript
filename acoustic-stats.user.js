eb3959c3448683e8bb4262e3af05be83b987b5b5
WaterGenie35 <thirdwatergenie@gmail.com>
2024-03-22 06:46:47 +0700

    Calculate quartiles

--- acoustic-stats.user.js
+++ acoustic-stats.user.js
@@ -26,12 +26,33 @@
                     .map(phone => phone.F[0])
                     .filter(freq => freq !== null)
                     .sort((f1, f2) => (+f1) - (+f2));
-                var minPitch = baseFrequencies[0];
-                var maxPitch = baseFrequencies[baseFrequencies.length - 1];
-                var rangePitch = maxPitch - minPitch;
+                var count = baseFrequencies.length;
 
-                console.log(`Min: ${minPitch}`);
-                console.log(`Max: ${maxPitch}`);
+                var q0Pitch = baseFrequencies[0];
+
+                var q1Pitch = 0;
+                var q3Pitch = 0;
+                var q1Index = count / 4.0;
+                var q3Index = 3 * q1Index;
+                if (count % 4 < 2) {
+                    q1Index = Math.round(q1Index);
+                    q3Index = Math.round(q3Index);
+                    q1Pitch = (baseFrequencies[q1Index - 1] + baseFrequencies[q1Index]) / 2;
+                    q3Pitch = (baseFrequencies[q3Index - 1] + baseFrequencies[q3Index]) / 2;
+                } else {
+                    q1Index = Math.floor(q1Index);
+                    q3Index = Math.floor(q3Index);
+                    q1Pitch = baseFrequencies[q1Index];
+                    q3Pitch = baseFrequencies[q3Index];
+                }
+                var q4Pitch = baseFrequencies[count - 1];
+                var rangePitch = q4Pitch - q0Pitch;
+
+                console.log(`Min: ${q0Pitch}`);
+                console.log(`Q1: ${q1Pitch}`);
+                console.log(`Q2: ${current.previewClip.medianPitch}`);
+                console.log(`Q3: ${q3Pitch}`);
+                console.log(`Max: ${q4Pitch}`);
                 console.log(`Range: ${rangePitch}`);
             }
             if (current.previewClip.transcript) {
