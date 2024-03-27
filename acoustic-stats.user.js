a43960e215693948b86951901de6029d4809c65e
WaterGenie35 <thirdwatergenie@gmail.com>
2024-03-27 22:50:00 +0700

    Save quantiles calculation

--- acoustic-stats.user.js
+++ acoustic-stats.user.js
@@ -8,6 +8,67 @@
 (function() {
     'use strict';
 
+    /**
+     * @param number[] sorted data
+     * @returns object containing number|undefined for q0, q1, q2, q3, and q4
+     */
+    function calculateQuantiles (data) {
+        var count = data.length;
+        if (count === 0) {
+            return {
+                q0: undefined,
+                q1: undefined,
+                q2: undefined,
+                q3: undefined,
+                q4: undefined
+            };
+        }
+        if (count === 1) {
+            return {
+                q0: data[0],
+                q1: data[0],
+                q2: data[0],
+                q3: data[0],
+                q4: data[0]
+            };
+        }
+
+        var q1 = null;
+        var q2 = null;
+        var q3 = null;
+
+        var q2Index = Math.floor(count / 2);
+        if (count % 2 === 1) {
+            q2 = data[q2Index];
+        } else {
+            q2 = (data[q2Index - 1] + data[q2Index]) / 2;
+        }
+
+        // count % 4 < 2 has even halves; count / 4 and 3 * count / 4 coincide with the upper of the 2 middle indices when rounded
+        // count % 4 >= 2 has odd halves; count / 4 and 3 * count / 4 coincide with the middle indices when floored
+        var q1Index = count / 4.0;
+        var q3Index = 3 * q1Index; // before rounding
+        if (count % 4 < 2) {
+            q1Index = Math.round(q1Index);
+            q3Index = Math.round(q3Index);
+            q1 = (data[q1Index - 1] + data[q1Index]) / 2;
+            q3 = (data[q3Index - 1] + data[q3Index]) / 2;
+        } else {
+            q1Index = Math.floor(q1Index);
+            q3Index = Math.floor(q3Index);
+            q1 = data[q1Index];
+            q3 = data[q3Index];
+        }
+
+        return {
+            q0: data[0],
+            q1: q1,
+            q2: q2,
+            q3: q3,
+            q4: data[count - 1]
+        };
+    }
+
     // Replaces the default renderer with additional stats
     globalState.render(['previewClip'], current => {
         if (current.previewClip) {
@@ -21,45 +82,38 @@
                 $('#preview-median-pitch').innerHTML = Math.round(current.previewClip.medianPitch) + 'Hz';
                 $('#preview-median-resonance').innerHTML = Math.round(current.previewClip.medianResonance * 100) + '%';
 
-                // save to previewClip instead, and check if already calculated
-                var baseFrequencies = current.previewClip.phones
-                    .map(phone => phone.F[0])
-                    .filter(freq => freq !== null)
-                    .sort((f1, f2) => (+f1) - (+f2));
-                var count = baseFrequencies.length;
-
-                var q0Pitch = baseFrequencies[0];
-
-                var q1Pitch = 0;
-                var q3Pitch = 0;
-
-                // count % 4 < 2 has even halves, count / 4 and 3 * count / 4 coincide with the upper of the 2 middle indices when rounded
-                // count % 4 >= 2 has odd halves, count / 4 and 3 * count / 4 coincide with the middle indices when floored
-                var q1Index = count / 4.0;
-                var q3Index = 3 * q1Index;
-                if (count % 4 < 2) {
-                    q1Index = Math.round(q1Index);
-                    q3Index = Math.round(q3Index);
-                    q1Pitch = (baseFrequencies[q1Index - 1] + baseFrequencies[q1Index]) / 2;
-                    q3Pitch = (baseFrequencies[q3Index - 1] + baseFrequencies[q3Index]) / 2;
-                } else {
-                    q1Index = Math.floor(q1Index);
-                    q3Index = Math.floor(q3Index);
-                    q1Pitch = baseFrequencies[q1Index];
-                    q3Pitch = baseFrequencies[q3Index];
+                var pitchQuantilesCalculated = (
+                    current.previewClip.minPitch &&
+                    current.previewClip.q1Pitch &&
+                    current.previewClip.q3Pitch &&
+                    current.previewClip.maxPitch &&
+                    current.previewClip.iqrPitch &&
+                    current.previewClip.rangePitch
+                );
+                if (!pitchQuantilesCalculated) {
+                    var fundamentalFrequencies = current.previewClip.phones
+                        .map(phone => phone.F[0])
+                        .filter(freq => freq !== null)
+                        .sort((f1, f2) => (+f1) - (+f2));
+                    var { q0, q1, _, q3, q4 } = calculateQuantiles(fundamentalFrequencies);
+                    var iqr = q3 - q1;
+                    var range = q4 - q0;
+                    current.previewClip.minPitch = q0;
+                    current.previewClip.q1Pitch = q1;
+                    current.previewClip.q3Pitch = q3;
+                    current.previewClip.maxPitch = q4;
+                    current.previewClip.iqrPitch = iqr;
+                    current.previewClip.rangePitch = range;
                 }
 
-                var q4Pitch = baseFrequencies[count - 1];
-                var iqrPitch = q3Pitch - q1Pitch;
-                var rangePitch = q4Pitch - q0Pitch;
-
-                console.log(`Min: ${q0Pitch}`);
-                console.log(`Q1: ${q1Pitch}`);
+                // add to DOM instead
+                console.log(`Min: ${current.previewClip.minPitch}`);
+                console.log(`Q1: ${current.previewClip.q1Pitch}`);
                 console.log(`Q2: ${current.previewClip.medianPitch}`);
-                console.log(`Q3: ${q3Pitch}`);
-                console.log(`Max: ${q4Pitch}`);
-                console.log(`IQR: ${iqrPitch}`);
-                console.log(`Range: ${rangePitch}`);
+                console.log(`Q3: ${current.previewClip.q3Pitch}`);
+                console.log(`Max: ${current.previewClip.maxPitch}`);
+                console.log(`IQR: ${current.previewClip.iqrPitch}`);
+                console.log(`Range: ${current.previewClip.rangePitch}`);
             }
             if (current.previewClip.transcript) {
                 $('#preview-transcript').innerHTML = current.previewClip.transcript;
@@ -67,6 +121,6 @@
         } else {
             $('.details').classList.remove('has-selection');
         }
-    })
+    });
 
 })();
